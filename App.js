9eccf2fe5361677fb20a87028db291059a0c2cd6
Sammamm <135961382+Sammamm@users.noreply.github.com>
2023-10-01 12:52:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AddImg from './components/AddImg';
+function App() {
+  return (
+    <div className="App">
+      <AddImg />
+    </div>
+  );
+}
+
+export default App;
